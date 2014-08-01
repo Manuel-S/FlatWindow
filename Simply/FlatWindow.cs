6f2429ca347a8ceb9b9ef9b60b88c81718a8d7cb
Manuel-S <schweigert@outlook.com>
2014-08-01 16:50:50 +0200

    Initial Commit

---
+++ Simply/FlatWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Simply
+{
+	public class FlatWindow : Window
+	{
+		#region Init
+
+		static FlatWindow()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatWindow),
+				new FrameworkPropertyMetadata(typeof(FlatWindow)));
+		}
+
+		#endregion
+	}
+}
